dfdb5ca49f08e3da446b80dd417c4b3510bb6483
Sharusan S <sharusans@Sharusans-MacBook-Air.local>
2024-05-04 22:16:07 +0530

    Basics

---
+++ Basics/operators.js
@@ -0,0 +1,14 @@
+var email= false;
+var FaceBook=false;
+var Google=false;
+
+// if(email && FaceBook && Google){
+//     console.log("Login Successful")
+// } 
+if(email || FaceBook || Google){
+    console.log("login successfull")
+}
+else{
+    console.log("Login failed")
+      
+}
