dbf738da936e1a7c04ade07223e958f39756d69e
Sharusan S <sharusans@Sharusans-MacBook-Air.local>
2024-05-08 19:20:00 +0530

    Intermediate/objects.js

---
+++ Intermediate/objects.js
@@ -0,0 +1,11 @@
+var mobile = {
+    Name : "Iphone",
+    Modelseries : "14plus",
+    Releasedyear: 2022,
+    Price : "Rs67000" ,
+    Madein : "India",
+    IosVersion : "17.4.1"
+}
+
+console.table(mobile);
+
